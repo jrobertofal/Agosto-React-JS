1309a47cd13763a35d47a3882fc287aa526d26bc
jrobertofal <roberto.fal19@gmail.com>
2022-08-26 14:26:23 -0600

    React-Nav-Bar

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+    return(
+    
+        <div className="header">
+            <div className="header__title">
+                <h1>Titulo</h1><em>(ReactJS)</em>
+            </div>
+                <div className="header__var">
+                    <ul>
+                        <li>Navegacion</li>
+                        <li>Navegacion</li>
+                        <li>Navegacion</li>
+                        <li>Navegacion</li>
+                    </ul>
+                </div>
+        </div>
+
+    );
+}
+
+export default Header;
